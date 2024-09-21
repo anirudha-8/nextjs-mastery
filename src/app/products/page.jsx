9df4ca4293bcaa91ac7c9fd3d709c10845befa8e
Anirudha <beleanirudha8@gmail.com>
2024-09-21 21:34:31 +0530

    create route in 'products' root

---
+++ src/app/products/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<>
+			<h1>Products</h1>
+			<hr />
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+				Quaerat aut in alias! Assumenda dignissimos rem fugit
+				accusantium natus reprehenderit doloribus voluptatem non,
+				mollitia hic facere maxime.
+			</p>
+		</>
+	);
+};
+
+export default page;
