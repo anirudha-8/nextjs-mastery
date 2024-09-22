304c101930ac267c16bcedede0482951808ac549
Anirudha <beleanirudha8@gmail.com>
2024-09-22 09:51:08 +0530

    create component for 'admin' section and add necessary routes

---
+++ src/app/admin/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<>
+			<h1>Admin Dashboard Section</h1>
+		</>
+	);
+};
+
+export default page;
---
+++ src/app/admin/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<>
+			<h1>Admin Profile Section</h1>
+		</>
+	);
+};
+
+export default page;
